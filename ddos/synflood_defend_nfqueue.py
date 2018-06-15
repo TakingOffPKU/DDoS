773770630e574c8c7f7015417a98be51d20697b5
liudexin <xtliudexin@gmail.com>
2018-06-15 10:48:05 +0800

    nfqueue

--- ddos/synflood_defend_nfqueue.py
+++ ddos/synflood_defend_nfqueue.py
@@ -1,73 +1,28 @@
-import netfilterqueue as nfqueue
+from netfilterqueue import NetfilterQueue
 from scapy.all import *
 import os
 
 # SERVERIP = "47.94.138.231"
 SERVERPORT = 8010
 
-# If you want to use it as a reverse proxy for your machine
-iptablesr = "iptables -A OUTPUT -j NFQUEUE"
 
-# If you want to use it for MITM :
-# iptablesr = "iptables -A FORWARD -j NFQUEUE"
-
-print("Adding iptable rules :")
-print(iptablesr)
-os.system(iptablesr)
-
-# If you want to use it for MITM attacks, set ip_forward=1 :
-#print("Set ipv4 forward settings : ")
-#os.system("sysctl net.ipv4.ip_forward=1")
-
-
-def callback(payload):
-    # Here is where the magic happens.
-    data = payload.get_data()
-    pkt = IP(data)
-    print("Got a packet ! source ip : " + str(pkt.src))
-    if pkt.port == SERVERPORT:
-        # Drop all packets coming from this IP
-        print("Dropped it. Oops")
-        payload.set_verdict(nfqueue.NF_DROP)
-    else:
-        # Let the rest go it's way
-        payload.set_verdict(nfqueue.NF_ACCEPT)
-    # If you want to modify the packet, copy and modify it with scapy then do :
-    #payload.set_verdict_modified(nfqueue.NF_ACCEPT, str(packet), len(packet))
-
+def print_and_accept(pkt):
+    print(pkt)
+    hw = pkt.get_hw()
+    if hw:
+        print(":".join("{:02x}".format(ord(c)) for c in hw[0:6]))
+    pkt.accept()
 
 def main():
-    # This is the intercept
-    q = nfqueue.queue()
-    q.open()
-    q.bind(socket.AF_INET)
-    q.set_callback(callback)
-    q.create_queue(0)
+    nfqueue = NetfilterQueue()
+    nfqueue.bind(1, print_and_accept)
     try:
-        q.try_run()  # Main loop
+        nfqueue.run()
     except KeyboardInterrupt:
-        q.unbind(socket.AF_INET)
-        q.close()
-        print("Flushing iptables.")
-        # This flushes everything, you might wanna be careful
-        os.system('iptables -F')
-        os.system('iptables -X')
+        print('Keyboard Interrrupt.')
+        exit(1)
 
-    # This is the intercept
-    q = nfqueue.queue()
-    q.open()
-    q.bind(socket.AF_INET)
-    q.set_callback(callback)
-    q.create_queue(0)
-    try:
-        q.try_run()  # Main loop
-    except KeyboardInterrupt:
-        q.unbind(socket.AF_INET)
-        q.close()
-        print("Flushing iptables.")
-        # This flushes everything, you might wanna be careful
-        os.system('iptables -F')
-        os.system('iptables -X')
+    nfqueue.unbind()
 
 
 if __name__ == "__main__":
