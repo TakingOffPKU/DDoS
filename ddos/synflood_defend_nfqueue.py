d4e80eebbd7eb2e54b0b3b47e73ef9276c3f25a9
liudexin <xtliudexin@gmail.com>
2018-06-15 15:16:58 +0800

    synch

--- ddos/synflood_defend_nfqueue.py
+++ ddos/synflood_defend_nfqueue.py
@@ -7,16 +7,30 @@ SERVERPORT = 8010
 iptables_init = 'iptables -I INPUT -p tcp --dport {} -j NFQUEUE --queue-num 1'.format(SERVERPORT)
 iptables_clean = 'iptables -F'
 
+white_list = set([])
+waitting_list = set([])
 
 def print_and_accept(pkt):
     hw = pkt.get_hw()
     if hw:
         ip_pkt = IP(pkt.get_payload())
         tcp_pkt = TCP(pkt.get_payload())
-        if(tcp_pkt.dport == SERVERPORT)
-            print('HTTP request from: {}:{}'.format(ip_pkt.src, tcp_pkt.sport))
-        print(tcp_pkt.payload)
-    pkt.accept()
+
+        src = ip_pkt.src
+        if src in white_list:
+            pkt.accept()
+        elif src in waitting_list:
+            if tcp_pkt.flags == 'A':
+                white_list.add(src)
+                waitting_list.remove(src)
+                pkt.accept()
+            else:
+                pkt.drop()
+        else:
+            if tcp_pkt.flags == 'S':
+                white_list.add(src)
+            else:
+                pkt.drop()
 
 def main():
     print('iptables init: {}'.format(iptables_init))
